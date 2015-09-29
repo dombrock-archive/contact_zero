6193bed79ca9a08578b373713227575c6d920695
Mathieu Dombrock <matdombrock@gmail.com>
2015-09-29 12:59:06 -0700

    Update view_db.php
    Updated requires to look in the /secure directory.

--- contact_zero/view_db.php
+++ contact_zero/view_db.php
@@ -1,17 +1,25 @@
 <?php
-require('connect.php');
-$sql ="SELECT * from CONTACTS;";
-
-   $ret = $db->query($sql);
-   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-      echo "ID = ". $row['ID'] . "\n"."<br>";
-      echo "NAME = ". $row['NAME'] ."\n"."<br>";
-      echo "EMAIL = ". $row['EMAIL'] ."\n"."<br>";
-      echo "PHONE = ". $row['PHONE'] ."\n"."<br>";
-      echo "SUBJECT = ". $row['SUBJECT'] ."\n"."<br>";
-      echo "MESSAGE = ". $row['MESSAGE'] ."\n"."<br>";
-      echo "<p>";
-
-   }
-   echo "Data queried successfully\n";
-   $db->close();
+require('secure/connect.php');
+require('secure/config.php');
+$pw = $_POST["pw"];
+if($pw == $contact_zero_config['Master_password']){
+  $sql ="SELECT * from CONTACTS;";
+  
+     $ret = $db->query($sql);
+     while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+        echo "<div class='contact_div'>";
+        echo "ID = ". $row['ID'] . "\n"."<br>";
+        echo "NAME = ". $row['NAME'] ."\n"."<br>";
+        echo "EMAIL = ". $row['EMAIL'] ."\n"."<br>";
+        echo "PHONE = ". $row['PHONE'] ."\n"."<br>";
+        echo "SUBJECT = ". $row['SUBJECT'] ."\n"."<br>";
+        echo "MESSAGE = ". $row['MESSAGE'] ."\n"."<br>";
+        echo "</div>";
+  
+     }
+     echo "Data queried successfully\n";
+     $db->close();
+}
+else{
+  echo "You didnt say the magic word!";
+}
