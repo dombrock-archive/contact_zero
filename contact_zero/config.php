f7322bd474abf5a88c4a2731321ac3eda2807d7c
Mathieu Dombrock <matdombrock@gmail.com>
2015-09-29 08:09:01 -0700

    Update config.php
    So... @email.com is a real email domain. Very sorry to all of the people I harassed while testing this.

--- contact_zero/config.php
+++ contact_zero/config.php
@@ -1,14 +1,13 @@
 <?php
 $contact_zero_config = array(
+  'Master_password'=>'Password123',//used for looking at the DB log
   'Master_email'=>'matdombrock@gmail.com',//This will be the main TO: address
-  'Master_backup' => true,//do we want to backup all messages to a SQLlite DB
-  'Master_password'=>'Password123',//used for looking at the DB log, if you are not using a DB backup ignore this
   'CC'=> array(
-    'joe'=>'joe@email.com',
-    'jane'=>'jane@email.com',
-    'jeff'=>'jeff@email.com',
-    'janette'=>'janette@email.com',
-    'new_USER'=>'new_USER@email.com'//can add new users
+    'joe'=>'joe@example.com',
+    'jane'=>'jane@example.com',
+    'jeff'=>'jeff@example.com',
+    'janette'=>'janette@example.com',
+    'new_USER'=>'new_USER@example.com'//can add new users
     )
 );
 //DO NOT EDIT BELOW THIS LINE
@@ -18,11 +17,11 @@ $contact_zero_config = array(
   'Master_password'=>'Password123',//used for looking at the DB log
   'Master_email'=>'mathieu@myceliumzero.com',
   'CC'=> array(
-    'joe'=>'joe@email.com',
-    'jane'=>'jane@email.com',
-    'jeff'=>'jeff@email.com',
-    'janette'=>'janette@email.com',
-    'new_USER'=>'new_USER@email.com'//can add new users
+    'joe'=>'joe@example.com',
+    'jane'=>'jane@example.com',
+    'jeff'=>'jeff@example.com',
+    'janette'=>'janette@example.com',
+    'new_USER'=>'new_USER@example.com'//can add new users
     )
 );
 */
